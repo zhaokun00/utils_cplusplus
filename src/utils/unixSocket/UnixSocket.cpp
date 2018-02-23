c8e5a41e5b627c48a2c16805f672f2ff26eb9c70
zhaokun <zhaokun@ainirobot.com>
2018-02-23 16:15:09 +0800

    docs:添加了unlink函数的注释信息

--- src/utils/unixSocket/UnixSocket.cpp
+++ src/utils/unixSocket/UnixSocket.cpp
@@ -6,6 +6,15 @@ int ipcOpen(const char * pPathName) {
    	struct sockaddr_un addr;
     int enable = 1;
 
+	/*
+		头文件：#include <unistd.h>
+		
+		定义函数：int unlink(const char * pathname);
+		
+		函数说明：unlink()会删除参数pathname 指定的文件. 如果该文件名为最后连接点, 但有其他进程打开了此文件, 则在所有关于此文件的文件描述词皆关闭后才会删除. 如果参数pathname 为一符号连接, 则此连接会被删除。
+		
+		返回值：成功则返回0, 失败返回-1, 错误原因存于errno
+	*/
 	unlink(pPathName);
 
    if ((socketFd = socket(AF_UNIX, SOCK_STREAM, 0)) != -1) {
