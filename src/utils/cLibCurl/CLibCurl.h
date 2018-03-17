dbb3c1d181d6396e6cd1d60edab927f6344b9cf2
zhaokun <zhaokun@ainirobot.com>
2018-03-17 16:10:19 +0800

    feature:添加了libcurl的上传文件功能,并且添加了get、post、下载文件的示例

--- src/utils/cLibCurl/CLibCurl.h
+++ src/utils/cLibCurl/CLibCurl.h
@@ -4,9 +4,27 @@
 	#include <curl/curl.h>
 	#include <string.h>
 	#include <string>
-	#include <unistd.h> 
-	using std::string;
+	#include <unistd.h>
+	#include <pthread.h>
+	#include <stdio.h>  
+	#include <sys/io.h>   	
+	#include <stdlib.h>  	
+	#include <errno.h>   	
+	#include <fcntl.h>  	
+	#include <sys/types.h>  	
+	#include <sys/socket.h>  	
+	#include <time.h>   	
+	#include <sys/stat.h>   	
+	#include <dirent.h>  	
+	#include <sys/inotify.h> 
 
+	#define USE_OPENSSL
+	#ifdef USE_OPENSSL
+		#include <openssl/crypto.h>
+	#endif
+	
+	using std::string;
+	
 	enum LibcurlMethod {
 	    Lm_None = 0,  
 	    Lm_Download,  
@@ -15,7 +33,7 @@
 	};
 
 	typedef void (*T)(const char *pData,size_t nSize);
-	typedef void (*E)(void *pParam, double total, double now);
+	typedef void (*E)(void *pParam, long long total, long long now);
 	
 	class CLibcurl {
 
@@ -34,25 +52,38 @@
 	    bool setCookie(const char * pCookie);                                 		     //设置HTTP请求cookie  
 	    bool setCookieFile(const char * pFilePath);                              			 //设置HTTP请求cookie文件    
 	    bool downloadToFile(const char * pUrl, const char *pFile,void * param,E callBack);                            //下载文件到磁盘  
-	    bool post(const char * pUrl, const char* pData,T callBack);   //Post字符串或者二进制数据  
-	    bool get(const char * pUrl,T callBack);                                                 //Get请求  
-	  
+		bool upLoadFile(const char *pUrl,const char *pFile); //上传文件
+		bool post(const char * pUrl, const char* pData,T callBack);   //Post字符串或者二进制数据  
+	    bool get(const char * pUrl,T callBack);                                                 //Get请求
+	    void closeFile();
+
+#ifdef USE_OPENSSL
+		static pthread_mutex_t *lockarray;
+		static void initLocks(void);
+	  	static void killLocks(void);
+		static unsigned long threadId(void);
+		static void lockCallback(int mode, int type, const char *file, int line);
+#endif 
+
 	protected:  
 	    static size_t   writeCallback(void* pBuffer, size_t nSize, size_t nmemb, void* pParam);  
-//	    static size_t   HeaderCallback(void* pBuffer, size_t nSize, size_t nMemByte, void* pParam);  
+//	    static size_t   HeaderCallback(void* pBuffer, size_t nSize, size_t nMemByte, void* pParam); 
+		//total:文件的大小 now:当前下载多少字节
 	    static int      progressCallback(void *pParam, double total, double now, double ultotal, double ulnow);  
 	  
 	private:  
 	    CURL			*m_pCurl; //curl的句柄  
 	    unsigned int    m_port;   //server的端口
-	    
-	    CURLcode 		m_curlCode; //执行libcurl返回码 
+
+		unsigned int 	m_connectionTimeOut; //定义连接时间
+		unsigned int    m_timeOut;           //定义读数据时间
+	    CURLcode 		m_curlCode;         //执行libcurl返回码 
 	    LibcurlMethod 	m_method;  
 	    curl_slist 		*m_curlList;
-		T 				m_response; //用户重写响应函数
-		FILE*  			m_File; //
-	    void    		*m_downLoadParam; //下载要传递的参数  
-		E    			m_downCallBack;  //用户重写下载进度函数
+		T 				m_response; 	    //用户重写响应函数
+		FILE*  			m_File; 		    //
+	    void    		*m_downLoadParam;   //下载要传递的参数  
+		E    			m_downCallBack;     //用户重写下载进度函数
 	};
 
 #endif
--- src/utils/cLibCurl/CLibcurl.cpp
+++ src/utils/cLibCurl/CLibcurl.cpp
@@ -1,8 +1,14 @@
 #include "CLibCurl.h"
 
+#ifdef USE_OPENSSL
+	pthread_mutex_t *CLibcurl::lockarray;
+#endif
+
 CLibcurl::CLibcurl()  
     : m_pCurl(NULL)  
     , m_port(80)
+    , m_connectionTimeOut(10)
+    , m_timeOut(10)
     , m_curlCode(CURLE_OK)
     , m_curlList(NULL) 
     , m_response(NULL)
@@ -12,12 +18,15 @@ CLibcurl::CLibcurl()
     
     m_pCurl = curl_easy_init();
 	curl_easy_setopt(m_pCurl, CURLOPT_NOSIGNAL, true);
+	curl_easy_setopt(m_pCurl, CURLOPT_CONNECTTIMEOUT, m_connectionTimeOut); //设置连接超时时间
+	curl_easy_setopt(m_pCurl, CURLOPT_TIMEOUT, m_timeOut); //设置超时时间
     curl_easy_setopt(m_pCurl, CURLOPT_WRITEFUNCTION, writeCallback); //设置回调函数  
     curl_easy_setopt(m_pCurl, CURLOPT_WRITEDATA, this); //将本身对象传递进去
 } 
 
 CLibcurl::~CLibcurl(void) {
 
+	closeFile();
 	clearHeaderList();	
 	curl_easy_cleanup(m_pCurl);
 }
@@ -31,17 +40,11 @@ bool CLibcurl::setPort(unsigned int port) {
 
 bool CLibcurl::setTimeout(unsigned int nSecond) {
 
-    if(nSecond<0)  
-        return false;
-	
     m_curlCode = curl_easy_setopt(m_pCurl, CURLOPT_TIMEOUT, nSecond); //设置执行超时时间
     return CURLE_OK == m_curlCode;  
 }
 
 bool CLibcurl::setConnectTimeout(unsigned int nSecond) {
-
-	if(nSecond<0)  
-        return false;
 
 	m_curlCode = curl_easy_setopt(m_pCurl, CURLOPT_CONNECTTIMEOUT, nSecond); //设置连接超时时间
     return CURLE_OK == m_curlCode;
@@ -143,13 +146,90 @@ bool CLibcurl::downloadToFile(const char * pUrl, const char *pFile,void * param,
         return false;  
     }  
     curl_easy_setopt(m_pCurl, CURLOPT_NOPROGRESS, 0);  
-    curl_easy_setopt(m_pCurl, CURLOPT_PROGRESSFUNCTION, progressCallback);  
-    curl_easy_setopt(m_pCurl, CURLOPT_PROGRESSDATA, this);  
+    curl_easy_setopt(m_pCurl, CURLOPT_PROGRESSFUNCTION, progressCallback); //设置进度回调函数
+    curl_easy_setopt(m_pCurl, CURLOPT_PROGRESSDATA, this); //在progressCallback回调函数中会将此参数传入进去
     m_method = Lm_Download;  
      
-    m_curlCode = curl_easy_perform(m_pCurl);  
+    m_curlCode = curl_easy_perform(m_pCurl);
+
     return CURLE_OK == m_curlCode;  
 }  
+
+bool CLibcurl::upLoadFile(const char *pUrl,const char *pFile) {
+
+	if((NULL == pUrl) || (NULL == pFile))
+		return false;
+
+	struct stat statBuff; 
+	char fileSize[64]={0};
+
+	if(stat(pFile, &statBuff) < 0) {
+		printf("%s don't exist = %s\n",pFile,strerror(errno));
+		return false;
+	}
+	else {
+		sprintf(fileSize, "%lu", statBuff.st_size);
+	}
+
+	struct curl_httppost *formpost = NULL;  
+  	struct curl_httppost *lastptr = NULL;  
+ 
+	curl_formadd(&formpost,  
+	
+				&lastptr,  
+	
+				CURLFORM_COPYNAME, "filename",	
+	
+				CURLFORM_FILE, pFile, //设置文件路径			   
+	
+				CURLFORM_CONTENTTYPE, "application/octet-stream",	
+	
+				CURLFORM_END);
+
+	curl_formadd(&formpost,  
+	
+				&lastptr,  
+	
+				CURLFORM_COPYNAME, "filesize",	
+	
+				CURLFORM_COPYCONTENTS, fileSize, //设置文件大小
+	
+				CURLFORM_END);	 
+	
+	curl_formadd(&formpost,  
+	
+				&lastptr,  
+	
+				CURLFORM_COPYNAME, "submit",  
+	
+				CURLFORM_COPYCONTENTS, "submit",  
+	
+				CURLFORM_END);
+
+	if(m_pCurl) {
+		
+		curl_easy_setopt(m_pCurl, CURLOPT_URL, pUrl);	
+		addHeader("Expect",""); //添加头信息		
+		curl_easy_setopt(m_pCurl, CURLOPT_HTTPPOST, formpost);  
+		
+		//curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, 1613);	
+		   		
+		m_curlCode = curl_easy_perform(m_pCurl);	 
+		
+		if(CURLE_OK != m_curlCode) {  		
+		  printf("curl url_easy_perform() failed:%s \n",curl_easy_strerror(m_curlCode));  
+		  return CURLE_OK == m_curlCode;
+		
+		}	
+
+		curl_formfree(formpost); 
+		return CURLE_OK == m_curlCode;
+
+	}
+
+	return CURLE_OK == m_curlCode;
+	
+}
 
 bool CLibcurl::post(const char * pUrl, const char* pData,T callBack) {  
 
@@ -158,7 +238,7 @@ bool CLibcurl::post(const char * pUrl, const char* pData,T callBack) {
 
 	m_response = callBack;
     curl_easy_setopt(m_pCurl, CURLOPT_POST, 1);  
-    curl_easy_setopt(m_pCurl, CURLOPT_POSTFIELDS, pData);  
+    curl_easy_setopt(m_pCurl, CURLOPT_POSTFIELDS, pData);
     curl_easy_setopt(m_pCurl, CURLOPT_POSTFIELDSIZE, strlen(pData));  
     curl_easy_setopt(m_pCurl, CURLOPT_URL, pUrl);
 
@@ -186,6 +266,14 @@ bool CLibcurl::get(const char * pUrl,T callBack) {
     return CURLE_OK == m_curlCode;  
 } 
 
+void CLibcurl::closeFile() {
+
+	if(m_File) {
+		fclose(m_File);
+		m_File = NULL;
+	}
+}
+
 size_t CLibcurl::writeCallback( void* pBuffer, size_t nSize, size_t nmemb, void* pParam) {  
 
     //把下载到的数据以追加的方式写入文件(一定要有a，否则前面写入的内容就会被覆盖了)  
@@ -212,11 +300,70 @@ size_t CLibcurl::writeCallback( void* pBuffer, size_t nSize, size_t nmemb, void*
 
 int CLibcurl::progressCallback(void *pParam, double total, double now, double ultotal, double ulnow) {
 
-    CLibcurl* pThis = (CLibcurl*)pParam;  
-    if (pThis->m_downCallBack) { 
-        pThis->m_downCallBack(pThis->m_downLoadParam, total, now);  
-    }  
+    CLibcurl* pThis = (CLibcurl*)pParam;
+
+	long long ltotal = (long long) total;
+	long long lnow = (long long) now;
+
+	if(ltotal > 0) {
+		if(ltotal == lnow) {
+			if(pThis->m_File) {
+				fflush(pThis->m_File); //当下载完成后重新刷数据到磁盘中
+			}
+		}
+		
+		if (pThis->m_downCallBack) { 
+			pThis->m_downCallBack(pThis->m_downLoadParam, ltotal, lnow);  
+		}  
+	}
+
+
     return 0;  
 }
+
+void CLibcurl::lockCallback(int mode, int type, const char *file, int line) {
+
+	(void)file; 
+ 	(void)line; 
+  	if(mode & CRYPTO_LOCK) { 
+    	pthread_mutex_lock(&(lockarray[type])); 
+  	} 
+  	else { 
+    	pthread_mutex_unlock(&(lockarray[type])); 
+  	} 
+} 
+       
+unsigned long CLibcurl::threadId(void) {
+
+	unsigned long ret; 
+
+	ret=(unsigned long)pthread_self(); 
+	return(ret); 
+} 
+       
+void CLibcurl::initLocks(void) {
+
+  int i; 
+   
+  lockarray=(pthread_mutex_t *)OPENSSL_malloc(CRYPTO_num_locks() *  sizeof(pthread_mutex_t));
+  
+  for (i=0; i<CRYPTO_num_locks(); i++) { 
+    pthread_mutex_init(&(lockarray[i]),NULL); 
+  } 
+   
+  CRYPTO_set_id_callback(threadId); 
+  CRYPTO_set_locking_callback(lockCallback); 
+} 
+       
+void CLibcurl::killLocks(void) {
+
+  int i; 
+   
+  CRYPTO_set_locking_callback(NULL); 
+  for (i=0; i<CRYPTO_num_locks(); i++) 
+    pthread_mutex_destroy(&(lockarray[i])); 
+   
+  OPENSSL_free(lockarray); 
+} 
 
 
