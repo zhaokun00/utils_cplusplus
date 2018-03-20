e967dd36ac5c982b9ea8126fee2b7d5e88408555
zhaokun <zhaokun@ainirobot.com>
2018-03-20 11:33:49 +0800

    feature:添加了glog的日志功能模块

--- src/utils/log/Log.cpp
+++ src/utils/log/Log.cpp
@@ -1,5 +1,7 @@
 #include "Log.h"
 
+LogUtils * LogUtils::logUtils = new LogUtils();
+
 static char LOG_TAG[LOG_LEVEL_NO][LOG_LEVEL_NAME] = {
 	"E",
 	"W",
@@ -78,5 +80,92 @@ void printfEx(const char * function,unsigned int level,const char *format, ...) 
 	} while(0);
 	
 }
+
+LogUtils::LogUtils() {
+
+}
+
+LogUtils::LogUtils(const LogUtils &) {
+
+}
+
+LogUtils& LogUtils::operator=(const LogUtils &LogUtils) {
+
+    return *this;
+}
+
+LogUtils * LogUtils::getInstance() {
+
+    if(logUtils != NULL) {
+        return logUtils;
+    }
+
+    return NULL;
+}
+
+void LogUtils::init(char * programName) {
+
+		google::InitGoogleLogging(programName);
+	
+		//设置是否将所有日志输出到stderr,而非文件,如果该项定义为true,则之间日志信息输出到屏幕上而不输出到日志中
+//		FLAGS_logtostderr = true;
+
+		//限制输出到stderr的部分信息,只有高于该级别的日志信息才会输出到屏幕中或者文件中
+		FLAGS_minloglevel = google::WARNING;
+	
+		//是否同时将日志输出到文件和stderr,该配置可以同时将日志信息输出到屏幕上和日志中,如果FLAGS_logtostderr = true;该项将不起作用
+		FLAGS_alsologtostderr = true;
+
+		//是否将不同级别的日志文件分割开来,该处为自己修改的源码,原始的日志文件低优先级的日志会包含高优先级的日志
+		FLAGS_servity_single_log = true;
+
+		//设置记录到标准输出的颜色消息,如果终端支持
+		FLAGS_colorlogtostderr = true;
+
+		//设置日志前缀是否应该添加到每行日志的输出,即日志格式是否输出,true为输出,false为不输出,日志格式自己也修改了源码
+//		FLAGS_log_prefix = false;
+
+		//缓冲日志输出，默认为30秒，此处改为0立即输出
+		FLAGS_logbufsecs = 0;
+
+		//设置最大日志大小,单位MB,当文件达到最大值时会重新再次创建一个新的文件
+		FLAGS_max_log_size = 1;
+
+		//当磁盘被写满时，停止日志输出
+		FLAGS_stop_logging_if_full_disk = true; 
+
+		//设置日志文件的输出目录,如果目录不存在则将不输出日志到文件
+//		FLAGS_log_dir = "/data/10.100.37.17/home/zhaok/test/utils_cplusplus/src/utils/log/";
+
+		//以下可以覆盖前面的日志路径
+#if 1
+		//设置google::FATAL 级别的日志存储路径和文件名前缀
+		google::SetLogDestination(google::GLOG_FATAL, "./log/fatal_");
+		//设置 google::ERROR 级别的日志存储路径和文件名前缀
+		google::SetLogDestination(google::GLOG_ERROR, "./log/error_");
+		//设置 google::WARNING 级别的日志存储路径和文件名前缀
+		google::SetLogDestination(google::GLOG_WARNING, "./log/warning_");
+		//设置 google::INFO 级别的日志
+		google::SetLogDestination(google::GLOG_INFO, "./log/info_");
+#endif
+
+		//设置文件扩展名,可以在文件名的后面添加该信息,可以用于区分不同的文件
+//		google::SetLogFilenameExtension("_extension_");
+
+		//设置捕捉 core dump
+		google::InstallFailureSignalHandler();
+		
+}
+
+void LogUtils::close() {
+
+	google::ShutdownGoogleLogging();
+
+	if(logUtils) {
+		delete logUtils;
+		logUtils = NULL;
+	}
+}
+
 
 
--- src/utils/log/Log.h
+++ src/utils/log/Log.h
@@ -7,7 +7,8 @@
 #include <string.h>
 #include <time.h>  
 #include <sys/time.h> 
-	
+#include <glog/logging.h>
+
 #define LOG_LEVEL_NO		4
 #define LOG_LEVEL_NAME		64
 
@@ -30,12 +31,36 @@ void printfEx(const char * function,unsigned int level,const char *format, ...);
 	#ifdef ANDROID
 		#define log(x...) ALOGD(x)
 	#else
-		#define log(level,format...) printfEx(__FUNCTION__,level,format)
+		//#define log(level,format...) printfEx(__FUNCTION__,level,format)
 		//define log(x...) printf(x)
+		#define log(x...) LOG(x)
 	#endif
 	
 #else
 	#define log(x...)
 #endif
+
+/*
+	glog中定义了4个级别:
+	enum SeverityLevel
+	{
+	　　google::INFO = 0,
+	　　google::WARNING = 1,
+	　　google::ERROR = 2,
+	　　google::FATAL = 3,
+	};
+*/
+class LogUtils {
+
+	public:
+	    static LogUtils * getInstance();
+		void init(char * programName);
+		void close();
+	private:
+		LogUtils();
+    	LogUtils(const LogUtils &);
+    	LogUtils & operator=(const LogUtils &);
+    	static LogUtils *logUtils;		
+};
 
 #endif
