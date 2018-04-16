e0b1d2be8d920886b42443acc1d70220e3ff154d
zhaokun <825537475@qq.com>
2018-04-16 23:35:14 +0800

    add string example

---
+++ src/utils/string/string.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <iostream>
+#include <thread>
+#include <string>
+#include <chrono>
+
+void test1_1(std::string s) {
+
+	s = "yangcuiling";
+
+	std::cout << s << std::endl;
+
+}
+
+void test1_2(std::string & s) {
+
+	s = "yangcuiling";
+
+	std::cout << s << std::endl;
+
+}
+
+std::string test1_3() {
+
+	std::string s = "yangcuiling";
+
+	return s;
+}
+void test1() {
+
+	std::string s = "zhaokun";
+
+//	test1_1(s);
+
+//	test1_2(s);
+
+	s = test1_3();
+
+	std::cout << s << std::endl;
+}
+
+
+int main() {
+
+	test1();
+//	test2();
+//	test3();
+//	test4();
+//	test5();
+//	test6();
+//	test7();
+//	test8();
+//	test9();
+//	test10();
+	return 0;
+}
+
