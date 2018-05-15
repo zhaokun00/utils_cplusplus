80971f689d237e8450c61bea86d2c37d0b440b89
zhaokun <825537475@qq.com>
2018-05-15 15:39:01 +0800

    feature:添加了cmake中的file命令

---
+++ src/utils/cMake/example_14/TestExample1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdint.h>
+#include <pthread.h>
+
+int main() {
+
+	printf("Hello CMake!\n");
+	return 0;
+}
+
